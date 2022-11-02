ce42b5e0808f6b82444b38b0be58ee10d47cfe32
Istavro Shakoori <Istavro@protonmail.com>
2022-11-02 17:15:45 +1100

    Add ability to check if anything changed in Downloads directory

--- directoryList.py
+++ directoryList.py
@@ -1,1 +1,4 @@
-test = "test1"
+# Use the same syntax as the example for directory shortcuts.
+# shortcut-name = "path/to/directory"
+
+documents = "~/Documents"
--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+# Importing some stuff.
+import os
+import time
+from pathlib import Path
 from directoryList import *
-print(test)
+
+# Get the path for the Downloads directory.
+downloads = str(Path.home()) + "/Downloads"
+
+# Check for changes in Downloads directory.
+while True:
+    NumberOfFiles=len(os.listdir(downloads))
+    time.sleep(20)
+    OldNumber = NumberOfFiles
+    NumberOfFiles = len(os.listdir(downloads))
+    if NumberOfFiles != OldNumber:
+        # Loop through the files and check if they have directories _ or # in the name to indicate a directory.
+        print("success")
